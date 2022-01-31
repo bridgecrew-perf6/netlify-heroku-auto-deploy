676fb3ea37b6d504fe96e2f48eeded9fc5b7d46f
Code Like Pro <ma@MAs-MacBook-Pro.local>
2022-01-31 20:04:50 +0100

    changed server message

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Welcome to our API'));
+app.get('/', (req, res) => res.send('Welcome to our API service'));
 
 app.listen(port, () => console.log(`Server is listening on port: ${port} `));
